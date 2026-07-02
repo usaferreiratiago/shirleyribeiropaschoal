e85b30057109adbce57f719f6f3a8b9b1ef23120
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-02 08:19:22 -0300

    shirleyribeiropaschoal

--- src/routes/funcionarios.tsx
+++ src/routes/funcionarios.tsx
@@ -40,7 +40,7 @@ const allStaff = [
     img: f5,
     name: "Shirley Ribeiro Paschoal",
     role: "Psicóloga",
-    info: "Psicóloga Clínica • Atendimento presencial e online",
+    info: "Psicóloga: Clínica • Atendimento presencial e online",
     year: "2011",
   },
   // {
