c2639edd14e056669c13156da95a58a6055571b1
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-02 08:04:58 -0300

    shirleyribeiropaschoal

--- src/routes/sobre.tsx
+++ src/routes/sobre.tsx
@@ -98,6 +98,7 @@ function Sobre() {
               {[
                 "CNPJ 38.039.512/0001-34.",
                 "Sede em Coronel Fabriciano/MG",
+                "CRP MG 36127",
                 "Equipe de psicólogos própria",
                 "Acompanhamento psicológico",
                 "Atuação em todo Vale do Aço",
