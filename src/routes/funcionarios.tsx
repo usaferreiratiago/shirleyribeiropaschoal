a7898f5effcb7a878a7073344642b5db49e86609
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-03 15:29:21 -0300

    shirleyribeiropaschoal

--- src/routes/funcionarios.tsx
+++ src/routes/funcionarios.tsx
@@ -40,7 +40,7 @@ const allStaff = [
     img: f5,
     name: "Shirley Ribeiro Paschoal",
     role: "Psicóloga",
-    info: "Psicóloga: Esp. em Terapia Cognitiva e Comportamental, Neuropsicóloga, Palestrante Clínica • Atendimento presencial e online",
+    info: "Esp. em Terapia Cognitiva e Comportamental, Neuropsicóloga, Palestrante Clínica • Atendimento presencial e online",
     year: "2011",
   },
   // {
