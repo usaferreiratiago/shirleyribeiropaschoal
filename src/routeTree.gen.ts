e3581be56ee8fc74ee5ee6c8425bf2e94ddce51c
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-04 07:14:37 -0300

    shirleyribeiropaschoal

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -18,7 +18,7 @@ const rootRoute = rootRouteImport
 const IndexRoute = IndexImport.update({ path: '/', getParentRoute: () => rootRoute } as any)
 const ClientesRoute = ClientesImport.update({ path: '/clientes', getParentRoute: () => rootRoute } as any)
 const ContatoRoute = ContatoImport.update({ path: '/contato', getParentRoute: () => rootRoute } as any)
-const FuncionariosRoute = FuncionariosImport.update({ path: '/funcionarios', getParentRoute: () => rootRoute } as any) // ADICIONADO
+const FuncionariosRoute = FuncionariosImport.update({ path: '/funcionarios', getParentRoute: () => rootRoute } as any)
 const PortfolioRoute = PortfolioImport.update({ path: '/portfolio', getParentRoute: () => rootRoute } as any)
 const ServicosRoute = ServicosImport.update({ path: '/servicos', getParentRoute: () => rootRoute } as any)
 const SobreRoute = SobreImport.update({ path: '/sobre', getParentRoute: () => rootRoute } as any)
