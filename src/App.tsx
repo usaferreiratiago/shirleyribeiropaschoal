d6112b9a060a7bca445f5c65731ddcb86563aa87
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-24 09:49:07 -0300

    shirleyribeiropaschoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable prettier/prettier */
-
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
+
+const router = getRouter();
 
 function App() {
   return <RouterProvider router={router} />;
--- src/router.tsx
+++ src/router.tsx
@@ -2,35 +2,21 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-// Fontes do Projeto
-import "@fontsource/space-grotesk/400.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/600.css";
-import "@fontsource/space-grotesk/700.css";
-import "@fontsource-variable/inter/index.css";
+export const getRouter = () => {
+  const queryClient = new QueryClient();
 
-/**
- * Instância única do QueryClient
- */
-export const queryClient = new QueryClient();
+  return createRouter({
+    routeTree,
+    context: {
+      queryClient,
+    },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+};
 
-/**
- * Instância única do Router
- */
-export const router = createRouter({
-  routeTree,
-  context: {
-    queryClient,
-  },
-  scrollRestoration: true,
-  defaultPreloadStaleTime: 0,
-});
-
-/**
- * Registro de tipos do TanStack Router
- */
 declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router;
+    router: ReturnType<typeof getRouter>;
   }
 }
